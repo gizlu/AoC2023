7c7e60ee2ab5404afd5c76a443f8b96c8b0787ab
gizlu <gizlu@mailbox.org>
2023-12-03 17:10:28 +0100

    Cleanup day3

--- day3/1.sh
+++ day3/1.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 sed 's/./\0 /g' <in | awk '
-function propagate(x, y) {
-  X[x, (y-1)]; X[x, (y+1)]; X[(x+1), y]; X[(x-1), y]; X[(x+1), (y-1)]; X[(x-1), (y+1)]; X[(x+1), (y+1)]; X[(x-1), (y-1)] 
-}
-{for(i=1;i<=NF;++i) {MAP[NR,i]=$i; if($i~/[^.0-9]/) propagate(NR, i)}}
-{MAX=NF}
-function propag_num(x, y) {
-  out =0; mult=1
-  if(!(MAP[x,y]~/[0-9]/) || (x,y) in DONE) return 0
-  for(i=y;i<=MAX&&MAP[x,i]~/[0-9]/;++i) {}
-  for(i=i-1;i>0&&MAP[x,i]~/[0-9]/;i--) {DONE[x,i]; out+=MAP[x,i]*mult; mult*=10}
-  # print x,y, out
+{for(i=1;i<=NF;++i) if($i~/[^.0-9]/) { # save coords for later inspection
+  X[NR, (i-1)]; X[NR, (i+1)]; X[(NR+1), i]; X[(NR-1), i]; 
+  X[(NR+1), (i-1)]; X[(NR-1), (i+1)]; X[(NR+1), (i+1)]; X[(NR-1), (i-1)] 
+}} {for(i=1;i<=NF;++i) MAP[NR,i]=$i}
+function slurp(x, y,    i) { # read and remove num from map
+  out=0;mult=1
+  if(!(MAP[x,y]~/[0-9]/)) return 0; # not a num
+  for(i=y;i<=NF&&MAP[x,i]~/[0-9]/;++i) {} # goto LSD
+  for(i=i-1;i>0&&MAP[x,i]~/[0-9]/;--i) {out+=MAP[x,i]*mult; mult*=10; MAP[x,i]="."}
   return out;
 }
-END { for(i in X) {split(i,sep,SUBSEP); s+=propag_num(sep[1],sep[2])} print s}'
-# END { for(i in MAP) if(i in X){print i, "tak"}else{print i,"nie"}}'
+END { for(i in X) {split(i,coords,SUBSEP); s+=slurp(coords[1],coords[2])} print s}'
--- day3/2.sh
+++ day3/2.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
-sed 's/./\0 /g'<in| awk '
-function propagate(x, y) {
-  X[x,y,x, (y-1)]; X[x,y,x, (y+1)]; X[x,y,(x+1), y]; X[x,y,(x-1), y]; X[x,y,(x+1), (y-1)]; X[x,y,(x-1), (y+1)]; X[x,y,(x+1), (y+1)]; X[x,y,(x-1), (y-1)] 
+sed 's/./\0 /g' <short | awk '
+{for(i=1;i<=NF;++i) if($i=="*") { # save cords for later inspection
+  GEAR_RATIO[NR,i]=1;
+  X[NR,i,NR,i-1]; X[NR,i,NR,i+1]; X[NR,i,NR+1,i]; X[NR,i,NR-1,i]; 
+  X[NR,i,NR+1,i-1]; X[NR,i,NR-1,i+1]; X[NR,i,NR+1,i+1]; X[NR,i,NR-1,i-1] 
+}} {for(i=1;i<=NF;++i) MAP[NR,i]=$i}
+function slurp(gear_x, gear_y, x, y,   i) { # read num, remove from map and update gear ratio
+  out=0;mult=1
+  if(!(MAP[x,y]~/[0-9]/)) return 0; # not a num
+  for(i=y;i<=NF&&MAP[x,i]~/[0-9]/;++i) {} # goto LSD
+  for(i=i-1;i>0&&MAP[x,i]~/[0-9]/;--i) {out+=MAP[x,i]*mult; mult*=10; MAP[x,i]="."}
+  PARTS[gear_x, gear_y]+=1;
+  GEAR_RATIO[gear_x, gear_y]*=out;
 }
-{for(i=1;i<=NF;++i) {MAP[NR,i]=$i; if($i=="*"){ GEAR_RATIO[NR,i]=1; propagate(NR, i)}}}
-{MAX=NF}
-function propag_num(x0,y0,x, y) {
-  # print x,y
-  out =0; mult=1
-  if(!(MAP[x,y]~/[0-9]/) || (x,y) in DONE) return 0
-  for(i=y;i<=MAX&&MAP[x,i]~/[0-9]/;++i) {}
-  for(i=i-1;i>0&&MAP[x,i]~/[0-9]/;i--) {DONE[x,i]; out+=MAP[x,i]*mult; mult*=10}
-  PARTS[x0,y0]+=1;
-  GEAR_RATIO[x0,y0]*=out;
-  # print("chuj"x0,y0,out,GEAR_RATIO[x0,y0])
-}
-END { for(i in X) {split(i,sep,SUBSEP); propag_num(sep[1],sep[2],sep[3],sep[4])}}
-END { for(i in GEAR_RATIO) {if(PARTS[i]==2) s+=GEAR_RATIO[i]}}
-END {print s}'
-# END { for(i in X) {split(i,sep,SUBSEP); s+=propag_num(sep[1],sep[2])} print s}'
-# END { for(i in MAP) if(i in X){print i, "tak"}else{print i,"nie"}}'
+END { for(i in X) {split(i,cords,SUBSEP); slurp(cords[1],cords[2],cords[3],cords[4])}}
+END { for(i in GEAR_RATIO)if(PARTS[i]==2) s+=GEAR_RATIO[i]} END {print s}'
