29613c09ab85b042110061a364edd4b878bc3efb
gizlu <git@gizlu.pl>
2024-01-09 22:32:29 +0100

    Cleanup day9

--- day9/1.sh
+++ day9/1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-awk '{print; while(!/^[0 ]*$/) {for(i=1;i<NF;++i) {$i=$(i+1)-$i} NF--; print;}}' <in.txt| awk '/^[0 ]*$/ {print sum; next} {sum+=$NF}' | tail -n1
+awk '{s+=$NF; while(!/^[0 ]*$/) {for(i=1;i<NF;++i) {$i=$(i+1)-$i} NF--; s+=$NF}} END{print s}' <in.txt
