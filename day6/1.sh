66fade6669991acdb61086fecf8ce58855025cee
gizlu <git@gizlu.pl>
2024-01-10 01:01:54 +0100

    Add day6/1

---
+++ day6/1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+awk '
+function ceil(x,     y){y=int(x); return(x>y?y+1:y)}
+function draw(t,v,d) {return -v*v + t*v == d}
+function calc(t,d){min=ceil((-t+sqrt(t**2-4*d))/(-2)); max=int((-t-sqrt(t**2-4*d))/(-2)); return max-min+1-draw(t,min,d)-draw(t,max,d)}
+NR==1 {for(i=2;i<=NF;++i) TIMES[i]=$i}
+NR==2 { mult=1; for(i=2;i<=NF;++i) {mult*=calc(TIMES[i], $i)} print mult}' <in.txt
